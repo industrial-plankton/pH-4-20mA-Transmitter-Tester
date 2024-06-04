f387a8e1349bfec4119142ad5e6310b25fbe08e1
Cam <cam@industrialplankton.com>
2024-06-04 10:38:32 -0700

    resistor things and more displayed. The 4-20 when it turns on is doing something weird to the analog pin that is adding a resistance from the pin to 0 V, it changes every time too. No idea why im just bumping up the good range for now

--- pHtransTester.ino
+++ pHtransTester.ino
@@ -24,14 +24,24 @@ void loop() {
   const int sensorValue = analogRead(A7);
 
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
-  const int pH = map(sensorValue, (1023 * 0.24 / 5), (1023 * 1.2 / 5), 0, 1400);  // using a 150 ohm resistor, so 4-20 mA maps to 0.6-3 V // ~60 ohm now so 0.24-1.2V
+const float resistance = 80; // This changes, for some reason the inrush 4-20 adds a resistance to the pin and it changes every time
+const float LowVoltage = 0.004 * resistance;
+const float HighVoltage = 0.020 * resistance;
+
+  const int pH = map(sensorValue, ((1023 * LowVoltage) / 5), ((1023 * HighVoltage) / 5), 0, 1400);  // using a 150 ohm resistor, so 4-20 mA maps to 0.6-3 V // ~60 ohm now so 0.24-1.2V
+  const int milivolts = map(sensorValue, 0, 1023, 0, 5000);
 
     lcd.setBacklight(255);
     lcd.home();
     lcd.clear();
     lcd.print(pH);
+    lcd.print(" : ");
+    lcd.print(sensorValue);
+    lcd.print(" : ");
+    lcd.print(milivolts);
+    lcd.print(" mV");
     lcd.setCursor(0, 1);
-    if (pH >= 450 && pH <= 550) {
+    if (pH >= 250 && pH <= 550) {
       lcd.print("good");
     } else {
       lcd.print("bad");
